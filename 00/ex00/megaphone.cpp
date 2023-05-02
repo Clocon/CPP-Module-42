9f286cf7903eaa5c0de9f9a1b70d2a220eb2cab2
Luis Francisco Morales Maldonado <luiste49@gmail.com>
2023-05-02 12:57:47 +0200

    00/ex00 done

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumorale <lumorale@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 13:06:52 by lumorale          #+#    #+#             */
+/*   Updated: 2023/05/01 13:52:05 by lumorale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv){
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *";
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+			if (i < argc - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
